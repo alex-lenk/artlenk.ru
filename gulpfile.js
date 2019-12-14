5e11681e9fa0a5c2eef4dc3102a261ca6b6c5a81
AlexLenk <alexlenk.ru@gmail.com>
2019-12-14 14:25:25 +0300

    Fixed gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,137 +1,138 @@
-'use strict';
-
 var gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    uglify = require('gulp-uglify'),
-    zip = require('gulp-zip'),
     sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    rigger = require('gulp-rigger'),
     browserSync = require('browser-sync'),
-    cleanCSS = require('gulp-clean-css'),
-    watch = require('gulp-watch'),
+    rigger = require('gulp-rigger'),
+    uglify = require('gulp-uglify'),
+    fileinclude = require('gulp-ex-file-include'),
+    cleancss = require('gulp-clean-css'),
     rename = require('gulp-rename'),
-    rimraf = require('rimraf'),
-    reload = browserSync.reload;
+    autoprefixer = require('gulp-autoprefixer'),
+    notify = require('gulp-notify'),
+    rsync = require('gulp-rsync'),
+    basePath = require('path'),
+    svgmin = require('gulp-svgmin'),
+    svgstore = require('gulp-svgstore');
 
 
-var path = {
-    build: { //Тут мы укажем куда складывать готовые после сборки файлы
-        html: './build/',
-        js: './build/js/',
-        css: './build/css/',
-        img: './build/img/',
-        maps: '../maps/',
-        fonts: './build/fonts/'
-    },
-    src: { //Пути откуда брать исходники
-        html: './src/html/**/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
-        js: './src/js/*.js',//В стилях и скриптах нам понадобятся только main файлы
-        style: './src/style/*.scss',
-        img: './src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
-        fonts: './src/fonts/**/*.*'
-    },
-    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
-        html: './src/html/**/*.html',
-        js: './src/js/**/*.js',
-        style: './src/style/**/*.scss',
-        img: './src/img/**/*.*',
-        fonts: './src/fonts/**/*.*'
-    },
-    clean: './build'
-};
-
-
-var config = {
-    server: {
-        baseDir: "./build"
-    },
-    tunnel: false,
-    host: 'localhost',
-    port: 9005,
-    logPrefix: "frontend",
-    devBaseUrl: 'http://localhost'
-};
-
-
-gulp.task('zip', function () {
-    gulp.src(['./build/*', '!./build/archive.zip'])
-        .pipe(zip('archive.zip'))
-        .pipe(gulp.dest('./build'));
-});
-gulp.task('webserver', function () {
-    browserSync(config);
-});
-
-
-gulp.task('clean', function (cb) {
-    rimraf(path.clean, cb);
-});
-
-gulp.task('html:build', function () {
-    gulp.src(path.src.html) //Выберем файлы по нужному пути
-        .pipe(rigger()) //Прогоним через rigger
-        .pipe(gulp.dest(path.build.html))//Выплюнем их в папку build
-        .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений*/
-});
-
-gulp.task('js:build', function () {
-    gulp.src(path.src.js) //Найдем наш main файл
-        .pipe(rigger()) //Прогоним через rigger
-        .pipe(uglify()) //Сожмем наш js
-        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
-        .pipe(reload({stream: true})); //И перезагрузим сервер
-});
-
-
-gulp.task('style:build', function () {
-    gulp.src(path.src.style) //Выберем наш main.scss
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'expanded'
-        }).on('error', sass.logError)) //Скомпилируем
-        .pipe(gulp.dest(path.build.css)) //И в build
-        .pipe(rename({
-            prefix: "",
-            suffix: ".min",
-            extname: ".css"
+gulp.task('svgIcons', function () {
+    return gulp
+        .src('./src/img/icons/*.svg')
+        .pipe(svgmin(function (file) {
+            var prefix = basePath.basename(file.relative, basePath.extname(file.relative));
+            return {
+                plugins: [{
+                    cleanupIDs: {
+                        prefix: prefix + '-',
+                        minify: true
+                    }
+                }]
+            }
         }))
-        .pipe(autoprefixer()) //Добавим вендорные префиксы
-        .pipe(cleanCSS({debug: true}, function (details) {
-        }))
-        .pipe(sourcemaps.write(path.build.maps, {
-            addComment: true
-            //sourceMappingURLPrefix: 'https://asset-host.example.com/assets'
-        }))
-        .pipe(gulp.dest(path.build.css)) //И в build
-        .pipe(reload({stream: true}));
+        .pipe(svgstore())
+        .pipe(gulp.dest('./build/img/'));
 });
 
-gulp.task('fonts:build', function () {
-    gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts))
+
+// Local Server
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: './build'
+        },
+        notify: false
+    })
 });
 
-gulp.task('build', [
-    'html:build',
-    'js:build',
-    'style:build',
-    'fonts:build'
-]);
+// SCSS Styles
+gulp.task('styles', function () {
+    return gulp.src('./src/scss/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}).on("error", notify.onError()))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer(['last 2 versions']))
+        .pipe(cleancss({level: {1: {specialComments: 0}}})) // Opt., comment out when debugging
+        .pipe(gulp.dest('./build/css'))
+        .pipe(browserSync.stream())
+});
+
+// JS
+gulp.task('scripts', function () {
+    return gulp.src([
+        './src/js/*.js'
+    ])
+        .pipe(rigger())
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(gulp.dest('./build/js'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+// HTML Live Reload
+gulp.task('code', function () {
+    return gulp.src([
+        './src/view/*.html'
+    ])
+        .pipe(fileinclude())
+        .pipe(gulp.dest('./build'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+// img task
+gulp.task('img', function () {
+    return gulp.src([
+        './src/img/**/*.*'
+    ])
+        .pipe(gulp.dest('./build/img'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+// favicon task
+gulp.task('favicon', function () {
+    return gulp.src([
+        './src/favicon/*.*'
+    ])
+        .pipe(gulp.dest('./build/favicon'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+// fonts task
+gulp.task('fonts', function () {
+    return gulp.src([
+        './src/fonts/*.*'
+    ])
+        .pipe(gulp.dest('./build/fonts'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+// Deploy
+gulp.task('rsync', function () {
+    return gulp.src('./src/**')
+        .pipe(rsync({
+            root: './build/',
+            hostname: 'artlenk.ru@gmail.com',
+            destination: 'yousite/public_html/',
+            // include: ['*.htaccess'], // Includes files to deploy
+            exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+            recursive: true,
+            archive: true,
+            silent: false,
+            compress: true
+        }))
+});
+
+
+gulp.task('img', gulp.parallel('img'));
+
+gulp.task('favicon', gulp.parallel('favicon'));
+
+gulp.task('fonts', gulp.parallel('fonts'));
 
 gulp.task('watch', function () {
-    watch([path.watch.html], function (event, cb) {
-        gulp.start('html:build');
-    });
-    watch([path.watch.style], function (event, cb) {
-        gulp.start('style:build');
-    });
-    watch([path.watch.js], function (event, cb) {
-        gulp.start('js:build');
-    });
-    watch([path.watch.fonts], function (event, cb) {
-        gulp.start('fonts:build');
-    });
+    gulp.watch('./src/scss/**/*.scss', gulp.parallel('styles'));
+    gulp.watch(['./src/js/**/*.js'], gulp.parallel('scripts'));
+    gulp.watch('./src/view/*.html', gulp.parallel('code'));
+    gulp.watch('./src/img/**/*', gulp.parallel('img'));
+    gulp.watch('./src/favicon/**/*', gulp.parallel('favicon'));
+    gulp.watch('./src/fonts/**/*', gulp.parallel('fonts'));
 });
 
-gulp.task('default', ['build', 'webserver', 'watch']);
+gulp.task('default', gulp.parallel('img', 'favicon', 'fonts', 'styles', 'scripts', 'code', 'browser-sync', 'watch'));
